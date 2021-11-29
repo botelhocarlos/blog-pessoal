834ca10b161d5a30f6931f83eaed3f5ca7dd449d
botelhocarlos <emaildocarlosbotelho@gmail.com>
2021-11-29 12:55:47 -0300

    Criando Projeto Blog Pessoal no React e criando o componente Home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src='https://i.imgur.com/2lhUi1S.png' alt='Minha imagem' className='img'/>
+        </>
+    );
+}
+
+export default Home;
