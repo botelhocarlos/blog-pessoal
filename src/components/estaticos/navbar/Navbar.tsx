ccb0357ee3ce7e1dd8597f0518afec7172d8265b
botelhocarlos <emaildocarlosbotelho@gmail.com>
2021-12-07 15:51:19 -0300

    Criando componentes Tema e Postagem com comunicação com o back end

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom'
+import useLocalStorage from 'react-use-localstorage';
+
 import './Navbar.css'
 function Navbar() {
+    const [token, setToken] = useLocalStorage('token');
+    let history = useHistory();
+
+    function goLogout() {
+        setToken('')
+        alert("Usuário deslogado")
+        history.push('/login')
+    }
     return (
         <>
             <AppBar position="static">
@@ -14,33 +25,40 @@ function Navbar() {
                     </Box>
 
                     <Box display="flex" justifyContent="start">
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                home
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                postagens
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                temas
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                cadastrar tema
-                            </Typography>
-                        </Box>
-                        <Link to='/login' className='text-decorator-none'>
+                        <Link to="/home" className="text-decorator-none">
                             <Box mx={1} className='cursor'>
                                 <Typography variant="h6" color="inherit">
-                                    logout
+                                    home
                                 </Typography>
                             </Box>
                         </Link>
+                        <Link to="/posts" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    postagens
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to="/temas" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    temas
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to="/formularioTema" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    cadastrar tema
+                                </Typography>
+                            </Box>
+                        </Link>
+
+                        <Box mx={1} className='cursor' onClick={goLogout}>
+                            <Typography variant="h6" color="inherit">
+                                logout
+                            </Typography>
+                        </Box>
 
                     </Box>
 
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -13,3 +13,8 @@ export const login = async (url: any, dados: any, setDado: any) => {
     const resposta = await api.post(url, dados)
     setDado(resposta.data.token)
 }
+
+export const busca = async (url: any, setDado: any, header: any) => {
+    const resposta = await api.get(url, header)
+    setDado(resposta.data)
+}
